11f0c9508b4598264ebb4d1737fefaaaf73ae08d
YamakawaMasanobu <hcu_yama@ca.info.hiroshima-cu.ac.jp>
2023-01-24 16:56:35 +0900

    クレジットマネジメントver1 Scanに前の値が残って永遠に初期入力画面とエラーをループする場合がある（再現方法がわからん）

---
+++ src/Credit_condition.java
@@ -0,0 +1,58 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Credit_condition {
+    boolean Nsors;  //New Sales Order Refusal State(新規伝票受付拒否状態)
+//    boolean Rotcc;  //Result of Temporary Credit Check(仮の与信チェックの結果):trueならUNDER,falseならOVER
+    boolean Cbf;    //Credit Block Flag(与信ブロックフラグ):trueならON,falseならOFF
+    boolean Cs;     //Credit State(与信状態):trueならUNDER,falseならOVER
+    double CreditLimit; //与信限度額
+
+    double Receivables; //債権
+
+    public void creditBlockFlagChange(boolean arg, Credit_condition voucher){
+        if (arg == true){
+            voucher.Cbf = true;
+            System.out.println("CreditBlockFlag = " + voucher.Cbf);
+        }else if (arg == false){
+            voucher.Cbf = false;
+            System.out.println("CreditBlockFlag = " + voucher.Cbf);
+        }
+    }
+
+    public void createSalesOrder(Credit_condition voucher, double newOrderAmount){
+        boolean resultNsorsChk = Main.newStateOrderRefusalStateCheck(voucher);
+
+        if(resultNsorsChk == true){
+            Main.errorMessageDisplay("You are in New sales order refusal state.");
+            System.out.println("receivables = " + voucher.Receivables);
+        }else if (resultNsorsChk == false && Main.temporaryCreditCheck(newOrderAmount, voucher) == false){
+            voucher.Receivables += newOrderAmount;
+            Main.errorMessageDisplay("Over credit limit");
+            System.out.println("receivables = " + voucher.Receivables);
+            creditBlockFlagChange(true, voucher);
+            voucher.Nsors = true;
+        }else if (resultNsorsChk == false && Main.temporaryCreditCheck(newOrderAmount, voucher) == true){
+            voucher.Receivables += newOrderAmount;
+            System.out.println("receivables = " + voucher.Receivables);
+            if(Main.creditBlockFlagCheck(voucher) == true){
+                creditBlockFlagChange(false, voucher);
+            }
+        }
+    }
+
+    public void raiseCreditLimit (Credit_condition voucher){
+        Scanner scan = new Scanner(System.in);
+        try {
+            System.out.println("Please enter the new maximum amount.");
+            double tmp = scan.nextDouble();
+            voucher.CreditLimit = tmp;
+            System.out.println("New credit limit: " + voucher.CreditLimit);
+            if (voucher.Receivables <= voucher.CreditLimit) {
+                creditBlockFlagChange(false, voucher);
+                voucher.Cs = true;
+            }
+        }catch (InputMismatchException e){
+            Main.errorMessageDisplay("A non-Double type value was entered.");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import javax.security.auth.login.CredentialNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Main {
+
+    static void errorMessageDisplay(String message) {
+        System.out.println("error: " + message);
+    }
+
+    static boolean temporaryCreditCheck(double amountOfMoney, Credit_condition voucher) {
+        double tmp = amountOfMoney + voucher.Receivables;
+        if (tmp <= voucher.CreditLimit) {
+            return true;
+        } else if (tmp > voucher.CreditLimit) {
+            voucher.Cs = false;
+            return false;
+        } else {
+            return false;
+        }
+    }
+
+    static boolean newStateOrderRefusalStateCheck(Credit_condition voucher) {
+        if (voucher.Cs == false && creditBlockFlagCheck(voucher) == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    static boolean creditBlockFlagCheck(Credit_condition voucher) {
+        return voucher.Cbf;
+    }
+
+    static void salesOrder(Credit_condition voucher, double orderAmount) {
+        voucher.createSalesOrder(voucher, orderAmount);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Credit_condition voucher_sample = new Credit_condition();
+        voucher_sample.CreditLimit = 1000;
+        voucher_sample.Nsors = false;
+        voucher_sample.Cbf = false;
+        voucher_sample.Cs = true;
+        voucher_sample.Receivables = 0;
+
+        while (true) {
+            try{
+                System.out.println("Sales order -> 1, Raise Credit Limit -> 2");
+                int tmp = scan.nextInt();
+                if (tmp == 1) {
+                    System.out.println("Please enter sales amount");
+                    salesOrder(voucher_sample, scan.nextDouble());
+                } else if (tmp == 2) {
+                    voucher_sample.raiseCreditLimit(voucher_sample);
+                } else{
+                    errorMessageDisplay("Please enter 1 or 2.");
+                }
+            }catch (InputMismatchException e) {
+                errorMessageDisplay("A non-Int type value was entered");
+            }
+        }
+    }
+}
